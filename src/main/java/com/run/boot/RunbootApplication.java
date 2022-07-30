c907c45b7a03124cf7de85d4831d36d12e7bde5a
maweijie <652518954@qq.com>
2022-07-31 01:06:05 +0800

    add run command check

--- src/main/java/com/run/boot/RunbootApplication.java
+++ src/main/java/com/run/boot/RunbootApplication.java
@@ -2,6 +2,8 @@ package com.run.boot;
 
 import com.run.boot.conf.RunCommandConfig;
 import com.run.boot.service.BaseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,6 +19,8 @@ import java.util.concurrent.ConcurrentHashMap;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class RunbootApplication implements CommandLineRunner {
 
+    private static final Logger LOG = LoggerFactory.getLogger(RunbootApplication.class);
+
     @Autowired
     private RunCommandConfig runCommands;
 
@@ -25,7 +29,7 @@ public class RunbootApplication implements CommandLineRunner {
 
     private Map<String, BaseService> baseServiceMap = new ConcurrentHashMap<>();
 
-    private static final String runCommandExample = "java -jar runboot-0.0.1-SNAPSHOT.jar 参数1  参数2  ...";
+    private static final String runCommandExample = "java -jar runboot-0.0.1-SNAPSHOT.jar param1  param2  ...";
 
     @PostConstruct
     public void initBaseService() {
@@ -41,12 +45,27 @@ public class RunbootApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        //todo 执行命令校验
+        if (args.length == 0) {
+            LOG.warn("No startup command is entered,program exited.");
+            return;
+        }
         Map<String, List<String>> commands = runCommands.getCommands();
-        for (String innerKey : commands.keySet()) {
-            List<String> innerCommands = commands.get(innerKey);
+        if (commands.size() == 0) {
+            LOG.warn("No runnable task was found,program exited.");
+            return;
+        }
+        LOG.info("Start executing command.");
+        for (int i = 0; i < args.length; i++) {
+            List<String> innerCommands = commands.get(args[i]);
+            LOG.info("Start executing group {} commands,command={},task list={}", i, args[i], innerCommands);
+            if (innerCommands.size() == 0) {
+                LOG.warn("This command does not create a task,skip command={}.", args[i]);
+                continue;
+            }
             for (String innerCommand : innerCommands) {
+                LOG.debug("Start task {}", innerCommand);
                 baseServiceMap.get(innerCommand).run();
+                LOG.debug("End task {}", innerCommand);
             }
         }
     }
